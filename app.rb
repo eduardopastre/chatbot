7fea0f4efaf28b8b07f526b23fdc116d9b21104b
Eduardo Pastre <dupastre@gmail.com>
2018-02-18 09:07:32 -0300

    create post /webhook route

--- app.rb
+++ app.rb
@@ -6,9 +6,26 @@ require './config/database'
 
 # Load Models
 Dir["./app/models/*.rb"].each {|file| require file }
+Dir["./app/services/**/*.rb"].each {|file| require file }
 
 class App < Sinatra::Base
   get '/' do
     'Hello world!'
+  end
+
+  post '/webhook' do
+    result = JSON.parse(request.body.read)["result"]
+    if result["contexts"].present?
+      response = InterpretService.call(result["action"], result["contexts"][0]["parameters"])
+    else
+      response = InterpretService.call(result["action"], result["parameters"])
+    end
+
+    content_type :json
+    {
+      "speech": response,
+      "displayText": response,
+      "source": "Slack"
+    }.to_json
   end
 end
