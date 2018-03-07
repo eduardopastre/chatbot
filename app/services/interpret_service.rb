97968a775891262268d8498ceacee6e745cbb31d
Eduardo Pastre <dupastre@gmail.com>
2018-03-07 20:56:59 -0300

    add new interpret command in interpret_service

--- app/services/interpret_service.rb
+++ app/services/interpret_service.rb
@@ -2,13 +2,21 @@ class InterpretService
   def self.call action, params
     case action
     when "list", "search", "search_by_hashtag"
-      FaqModule::ListService.new(params, action).call()
+      FaqModule::ListService.new(params, action).call
     when "create"
-      FaqModule::CreateService.new(params).call()
+      FaqModule::CreateService.new(params).call
     when "remove"
-      FaqModule::RemoveService.new(params).call()
+      FaqModule::RemoveService.new(params).call
     when "help"
       HelpService.call()
+    when "create_user"
+      UserModule::CreateService.new(param).call
+    when "favorites"
+      UserFaqModule::ListService.new(param).call
+    when "add_favorite"
+      UserFaqModule::CreateService.new(params).call
+    when "remove_favorte"
+      UserFaqModule::RemoveService.new(param).call
     else
       "Não compreendi o seu desejo"
     end
