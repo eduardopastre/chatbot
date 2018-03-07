dbb06f966b16862b93e9525acaf431cfbce0d530
Eduardo Pastre <dupastre@gmail.com>
2018-03-07 07:08:29 -0300

    test list_service user_faq

--- spec/service/user_faq/list_service_spec.rb
+++ spec/service/user_faq/list_service_spec.rb
@@ -1,5 +1,45 @@
 require_relative './../../spec_helper.rb'
 
-describe UserModule::ListService do
+describe UserFaqModule::ListService do
+  describe "#call" do
+    before(:each) do
+      @user = create(:user)
+      @faq1 = create(:faq)
+      @faq2 = create(:faq)
+      @faq3 = create(:faq)
+    end
+    it "Without param username, receive username required" do
+      list_service = UserFaqModule::ListService.new({})
+      response = list_service.call()
+      expect(response).to match('Usuário não encontrado')
+    end
 
+    it "With invalid username, receive user not found" do
+      username = FFaker::Name.first_name
+      list_service = UserFaqModule::ListService.new({'original-username' => username})
+      response = list_service.call()
+      expect(response).to match('Usuário não encontrado')
+    end
+
+    it "Without favorite faq, receive not find favorites" do
+      list_service = UserFaqModule::ListService.new({'original-username' => @user.username})
+      response = list_service.call()
+      expect(response).to match('Nenhum favorito encontrado')
+    end
+
+    it "With one favorite, receive list of faqs" do
+      create(:user_faq, user: @user, faq: @faq1)
+      list_service = UserFaqModule::ListService.new({'original-username' => @user.username})
+      response = list_service.call()
+      expect(response.length).to eq(1)
+    end
+
+    it "With two or more favorite, receive list of faqs" do
+      create(:user_faq, user: @user, faq: @faq1)
+      create(:user_faq, user: @user, faq: @faq2)
+      list_service = UserFaqModule::ListService.new({'original-username' => @user.username})
+      response = list_service.call()
+      expect(response.length).to eq(2)
+    end
+  end
 end
--- spec/service/user_faq/remove_service_spec.rb
+++ spec/service/user_faq/remove_service_spec.rb
@@ -1,5 +1,21 @@
 require_relative './../../spec_helper.rb'
 
-describe UserModule::RemoveService do
+describe UserFaqModule::RemoveService do
+  describe "#call" do
+    it "Without username, receive username required" do
 
+    end
+
+    it "Without faq_id, receive faq required" do
+
+    end
+
+    it "Whit invalid faq, receive faq not found" do
+
+    end
+
+    it "Whit valid faq_id, remove from favorites" do
+
+    end
+  end
 end
