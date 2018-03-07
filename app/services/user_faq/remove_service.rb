01fbc5608b512486ba74a36e1a5ae627dea3fd82
Eduardo Pastre <dupastre@gmail.com>
2018-03-07 13:40:52 -0300

    create user_faq remove_service

---
+++ app/services/user_faq/remove_service.rb
@@ -0,0 +1,25 @@
+module UserFaqModule
+  class RemoveService
+    def initialize(params)
+      @user = User.find_by_username(params['original-username'])
+      @faq_id = params['original-faq']
+    end
+
+    def call
+      return 'Username não encontrado' if @user.nil?
+      return 'Informe o id faq' if @faq_id.nil?
+
+      begin
+        user_faq = UserFaq.find_by_user_id_and_faq_id(@user.id, @faq_id)
+        if !user_faq.nil?
+          user_faq.delete
+          'Favorito removido com sucesso'
+        else
+          'Favorito não encontrado'
+        end
+      rescue
+        'Falha ao tentar remover favorito'
+      end
+    end
+  end
+end
